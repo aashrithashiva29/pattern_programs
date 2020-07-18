e317e71a33072fab197c65d2c7dc6db53232d9f7
aashrithashiva29 <vijayashiva70@gmail.com>
2020-07-18 20:28:12 +0530

    initial commit

---
+++ patterns/hollow_half_pyramid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            if(i==j || i==n || j==1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+/*
+input:-5
+output:- 
+*                                                                    
+* *                                                                  
+*   *                                                                
+*      *                                                              
+* * * * * 
+
+*/
